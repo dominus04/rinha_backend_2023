8f9e94bd35b43ea274f7942bafc6de9e2f9bd493
Dominus04 <igorctmdt@hotmail.com>
2024-03-24 20:57:25 -0300

    Corrigindo entrada de data no postgres e aumentando número de workers no nginx

--- init.sql
+++ init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     apelido VARCHAR(32) NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
-    nascimento DATE NOT NULL,
+    nascimento varchar(10) NOT NULL,
     stack VARCHAR(1024),
     campo_busca TEXT GENERATED ALWAYS AS (LOWER(apelido || nome || stack)) STORED
 );
