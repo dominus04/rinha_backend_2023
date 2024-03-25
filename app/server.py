b33c33275b422617ef9cd60b9f5fd95b7d2f1ae8
Dominus04 <igorctmdt@hotmail.com>
2024-03-24 22:59:53 -0300

    Escondendo os logs do HTTP server

--- app/server.py
+++ app/server.py
@@ -14,7 +14,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     
     def log_message(self, format, *args):
         message = (format % args).translate(self._control_char_table)
-        if ' 502' in message:
+        if ' 200' not in message or ' 400' not in message or ' 402' not in message or ' 201' not in message or ' 404' not in message:
             sys.stderr.write("%s\n" %(message))
     
     def set_response(self, status_code, content_type = None, location = None, content=None):
